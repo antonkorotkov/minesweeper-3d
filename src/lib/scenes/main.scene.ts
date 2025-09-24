01e0e130758e5ed6d76477efdc600de21f899fc0
Anton Korotkov <antonkorotkoff@gmail.com>
2025-09-24 09:21:51 +0200

    stats lib replace

--- src/lib/scenes/main.scene.ts
+++ src/lib/scenes/main.scene.ts
@@ -4,7 +4,7 @@ import {
     Ticker as PixiTicker,
     Container as PixiContainer
 } from 'pixi.js';
-import { Stats } from 'stats.ts';
+import Stats from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import TWEEN from 'three/examples/jsm/libs/tween.module.js';
 import AssetsLoader from '../assets/assets.loader';
